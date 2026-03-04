7a446eda44ad1335e61654dbec2a2d6193eff07c
nsapscience <sappecknoel@gmail.com>
2026-03-04 22:14:20 +0100

    Keine Ahnung

--- train_model.py
+++ train_model.py
@@ -3,8 +3,8 @@ from ultralytics import YOLO #import von YOLO
 model = YOLO("yolov8s.pt") #Pfad zur zu trainierenden KI
 
 model.train(
-    data = r"E:\Seminararbeit\dataset\data.yaml", #Pfad zur data.yaml
-    epochs = 75, #Anzahl der Durchläufe
+    data = r"E:\Datenbank\data.yaml", #Pfad zur data.yaml
+    epochs = 60, #Anzahl der Durchläufe
     imgsz = 640, #Bildgröße (Standard für YOLO)
     augment = True #Trainingsgenauigkeit verbessern
 )
